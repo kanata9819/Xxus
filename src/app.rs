96d6315c1e4d378c76c7ff3edb54b47da2633e81
kanata9819 <taka9819@gmail.com>
2025-08-30 23:27:47 +0900

    不要コード削除

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,8 @@
 use crate::enums::enum_global::AppRoute;
 use dioxus::prelude::*;
 use dioxus_router::prelude::Router;
-use wasm_bindgen::prelude::*;
 
 static CSS_PATH: Asset = asset!("/assets/styles.css");
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
 
 #[component]
 pub fn App() -> Element {
